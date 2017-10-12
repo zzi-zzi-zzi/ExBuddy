0c09a7f3cf05db765b8862e6377078f7ad11325b
Entrax <entrax.643@gmail.com>
2017-10-12 05:30:20 +0200

    version update

--- ExBuddy/Properties/AssemblyInfo.cs
+++ ExBuddy/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.0.*")]
+[assembly: AssemblyVersion("4.1.*")]
 #pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
-[assembly: AssemblyFileVersion("4.0.6.1")]
+[assembly: AssemblyFileVersion("4.1.0.0")]
 #pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
